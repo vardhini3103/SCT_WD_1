9a4255b482bf9b1b11ae0c8c4f843545f1b7ab49
Harshavardhini G <120570575+vardhini3103@users.noreply.github.com>
2024-09-01 15:06:30 +0530

    responsive landing page

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
+    } else {
+        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+    }
+});
